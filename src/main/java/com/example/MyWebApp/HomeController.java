0ed6434b153093b230d1be25c556c3ed46998775
Mani Maaran <manimaran4120@gail.com>
2026-08-04 14:42:29 +0530

    summa

--- src/main/java/com/example/MyWebApp/HomeController.java
+++ src/main/java/com/example/MyWebApp/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "--❤️! My Spring Boot Application is Running Successfully❤️--";
+        return "--My Spring Boot Application is Running Successfully--";
     }
 }
