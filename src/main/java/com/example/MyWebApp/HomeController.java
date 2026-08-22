f50a5ba0e3b4db8fcf829607d7489b2ef9e6fafb
Mani Maaran <manimaran4120@gail.com>
2026-08-22 23:01:17 +0530

    updated project

--- src/main/java/com/example/MyWebApp/HomeController.java
+++ src/main/java/com/example/MyWebApp/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "My Spring Boot Application is Running Successfully. Thank you UsersSSSSSS^^^^";
+        return "My Spring Boot Application is Running Successfully. Thank you Customers for using our application.";
     }
 }
